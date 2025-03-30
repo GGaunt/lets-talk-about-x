0f6e80bdfd6b80aeece8997b639910b24d670672
G.Gaunt <153116831+GGaunt@users.noreply.github.com>
2025-03-30 22:33:22 +0100

    added deleted_at to sql

--- backend/setup/setupDB.sql
+++ backend/setup/setupDB.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS `user_post`;
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user`
 (
@@ -15,17 +16,17 @@ VALUES ('joshname', 'joshpass', 'josh@josh.co.uk', 'admin'),
        ('garryname', 'garrypass', 'garry@garry.co.uk', 'user');
 
 
-DROP TABLE IF EXISTS `user_post`;
 CREATE TABLE IF NOT EXISTS `user_post`
 (
     `id`      int(11)      NOT NULL AUTO_INCREMENT,
     `user_id` int(11)      NOT NULL,
     `content` text         NOT NULL,
+    `deleted_at` datetime      NULL,
     PRIMARY KEY (`id`),
     FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
-INSERT INTO `user_post` (user_id, content)
-VALUES (1, 'This is a post by Josh (cool)'),
-       (2, 'This is a post by Garry (bad)');
+INSERT INTO `user_post` (user_id, content, deleted_at)
+VALUES (1, 'This is a post by Josh (cool)', NULL),
+       (2, 'This is a post by Garry (bad)', now());
