8435236f3d6a7c46aeb9204133531978f37e7228
G.Gaunt <153116831+GGaunt@users.noreply.github.com>
2025-03-30 21:55:38 +0100

    refactored backend, created load posts function

--- frontend/public/index.js
+++ frontend/public/index.js
@@ -1,8 +1,24 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     fetch("http://localhost:3000/users")
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log("Users:", data);
+//         })
+//         .catch(error => console.error("Error fetching users:", error));
+// });
+
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("http://localhost:3000/users")
+    fetch("http://localhost:3000/posts")
         .then(response => response.json())
         .then(data => {
-            console.log("Users:", data);
+            console.log("Posts:", data);
+            loadPosts(data);
         })
-        .catch(error => console.error("Error fetching users:", error));
+        .catch(error => console.error("Error fetching posts:", error));
 });
+
+
+
+function loadPosts(data) {
+    console.log(data)
+};
